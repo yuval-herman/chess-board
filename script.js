2dd5ee8d3d8da58002745d377cf1b7793e644a4b
yuval <nhajeho1@gmail.com>
2022-04-14 12:57:01 +0300

    new js to generate board

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    let board = document.createElement('table');
+    document.getElementById("board").appendChild(board);
+    for (let i = 0; i < 8; i++) {
+        let row = document.createElement('tr');
+        for (let j = 0; j < 8; j++) {
+            row.appendChild(document.createElement('td'))
+        }
+        board.appendChild(row)
+    }
+})
