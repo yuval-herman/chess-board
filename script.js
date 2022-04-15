f33efdffdcd10b2b24979ec0a9ce26583bc293d2
yuval <nhajeho1@gmail.com>
2022-04-15 12:00:35 +0300

    format js file

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 window.addEventListener('load', () => {
     let board = document.createElement('table');
+
     board.className = "board"
-    document.getElementById("board").appendChild(board);
+    document.getElementsByTagName('body')[0].appendChild(board);
+
     for (let i = 0; i < 8; i++) {
         let row = document.createElement('tr');
+
         for (let j = 0; j < 8; j++) {
-            row.appendChild(document.createElement('td'))
+            row.appendChild(document.createElement('td'));
         }
-        board.appendChild(row)
+
+        board.appendChild(row);
     }
 })
+//js by reference vs by value
+let a=1;
+let b=a;
+b=2
+console.log(a);
