8dec4c3a003fa5931d07cb4b1d753fd0520c741e
yuval <nhajeho1@gmail.com>
2022-04-17 11:21:20 +0300

    remove unneeded load event from js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-window.addEventListener('load', () => {
+const BOARD_SIZE = 8;
+
+function makeBoard() {
     let board = document.createElement('table');
 
     board.className = "board"
     document.getElementsByTagName('body')[0].appendChild(board);
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < BOARD_SIZE; i++) {
         let row = document.createElement('tr');
 
-        for (let j = 0; j < 8; j++) {
+        for (let j = 0; j < BOARD_SIZE; j++) {
             row.appendChild(document.createElement('td'));
         }
 
         board.appendChild(row);
     }
-})
+}
+
+makeBoard()
