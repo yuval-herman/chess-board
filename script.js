d055ad870d9134c41a7720bea54768bfec574042
yuval <nhajeho1@gmail.com>
2022-04-17 22:11:10 +0300

    formating

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function makeBoard() {
 
         for (let j = 0; j < BOARD_SIZE; j++) {
             let td = document.createElement('td');
-            td.onclick = (td) => {cellClick(td)};
+            td.onclick = (td) => { cellClick(td) };
             row.appendChild(td);
         }
 
@@ -25,7 +25,7 @@ function makeBoard() {
 }
 
 function populateBoard(board) {
-    getCell = (x,y) => {
+    getCell = (x, y) => {
         return board.rows[x].cells[y];
     }
 
@@ -35,32 +35,32 @@ function populateBoard(board) {
         img.className = "piece";
         return img;
     }
-    
-    getCell(0,0).appendChild(makePiece('Chess_rdt45'));
-    getCell(0,1).appendChild(makePiece('Chess_ndt45'));
-    getCell(0,2).appendChild(makePiece('Chess_bdt45'));
-    getCell(0,3).appendChild(makePiece('Chess_qdt45'));
-    getCell(0,4).appendChild(makePiece('Chess_kdt45'));
-    getCell(0,5).appendChild(makePiece('Chess_bdt45'));
-    getCell(0,6).appendChild(makePiece('Chess_ndt45'));
-    getCell(0,7).appendChild(makePiece('Chess_rdt45'));
+
+    getCell(0, 0).appendChild(makePiece('Chess_rdt45'));
+    getCell(0, 1).appendChild(makePiece('Chess_ndt45'));
+    getCell(0, 2).appendChild(makePiece('Chess_bdt45'));
+    getCell(0, 3).appendChild(makePiece('Chess_qdt45'));
+    getCell(0, 4).appendChild(makePiece('Chess_kdt45'));
+    getCell(0, 5).appendChild(makePiece('Chess_bdt45'));
+    getCell(0, 6).appendChild(makePiece('Chess_ndt45'));
+    getCell(0, 7).appendChild(makePiece('Chess_rdt45'));
 
     for (let i = 0; i < BOARD_SIZE; i++) {
-        getCell(1,i).appendChild(makePiece('Chess_pdt45'));
+        getCell(1, i).appendChild(makePiece('Chess_pdt45'));
     }
 
     for (let i = 0; i < BOARD_SIZE; i++) {
-        getCell(6,i).appendChild(makePiece('Chess_plt45'));
+        getCell(6, i).appendChild(makePiece('Chess_plt45'));
     }
 
-    getCell(7,0).appendChild(makePiece('Chess_rlt45'));
-    getCell(7,1).appendChild(makePiece('Chess_nlt45'));
-    getCell(7,2).appendChild(makePiece('Chess_blt45'));
-    getCell(7,3).appendChild(makePiece('Chess_qlt45'));
-    getCell(7,4).appendChild(makePiece('Chess_klt45'));
-    getCell(7,5).appendChild(makePiece('Chess_blt45'));
-    getCell(7,6).appendChild(makePiece('Chess_nlt45'));
-    getCell(7,7).appendChild(makePiece('Chess_rlt45'));
+    getCell(7, 0).appendChild(makePiece('Chess_rlt45'));
+    getCell(7, 1).appendChild(makePiece('Chess_nlt45'));
+    getCell(7, 2).appendChild(makePiece('Chess_blt45'));
+    getCell(7, 3).appendChild(makePiece('Chess_qlt45'));
+    getCell(7, 4).appendChild(makePiece('Chess_klt45'));
+    getCell(7, 5).appendChild(makePiece('Chess_blt45'));
+    getCell(7, 6).appendChild(makePiece('Chess_nlt45'));
+    getCell(7, 7).appendChild(makePiece('Chess_rlt45'));
 }
 
 function cellClick(event) {
@@ -74,6 +74,7 @@ function cellClick(event) {
     prevCell = cell;
     prevColor = cell.style.background;
     cell.style.background = HIGHLIGHT_COLOR;
+
 }
 
 let board = makeBoard();
