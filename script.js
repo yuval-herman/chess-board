e79ca400c8ae2f8ca120b5b4307f0b7d396b22e9
yuval <nhajeho1@gmail.com>
2022-04-17 13:43:01 +0300

    script.js: added logic to populateBoard function

--- script.js
+++ script.js
@@ -20,14 +20,42 @@ function makeBoard() {
 }
 
 function populateBoard(board) {
-    for (const row of board.rows) {
-        for (const cell of row.cells) {
-            let img = document.createElement('img');
-            img.src = "pieces/Chess_plt45.svg"
-            img.className = "piece"
-            cell.appendChild(img)
-        }
+    getCell = (x,y) => {
+        return board.rows[x].cells[y];
     }
+
+    makePiece = (name) => {
+        let img = document.createElement('img');
+        img.src = "pieces/" + name + ".svg";
+        img.className = "piece";
+        return img;
+    }
+    
+    getCell(0,0).appendChild(makePiece('Chess_rdt45'));
+    getCell(0,1).appendChild(makePiece('Chess_ndt45'));
+    getCell(0,2).appendChild(makePiece('Chess_bdt45'));
+    getCell(0,3).appendChild(makePiece('Chess_qdt45'));
+    getCell(0,4).appendChild(makePiece('Chess_kdt45'));
+    getCell(0,5).appendChild(makePiece('Chess_bdt45'));
+    getCell(0,6).appendChild(makePiece('Chess_ndt45'));
+    getCell(0,7).appendChild(makePiece('Chess_rdt45'));
+
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        getCell(1,i).appendChild(makePiece('Chess_pdt45'));
+    }
+
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        getCell(6,i).appendChild(makePiece('Chess_plt45'));
+    }
+
+    getCell(7,0).appendChild(makePiece('Chess_rlt45'));
+    getCell(7,1).appendChild(makePiece('Chess_nlt45'));
+    getCell(7,2).appendChild(makePiece('Chess_blt45'));
+    getCell(7,3).appendChild(makePiece('Chess_qlt45'));
+    getCell(7,4).appendChild(makePiece('Chess_klt45'));
+    getCell(7,5).appendChild(makePiece('Chess_blt45'));
+    getCell(7,6).appendChild(makePiece('Chess_nlt45'));
+    getCell(7,7).appendChild(makePiece('Chess_rlt45'));
 }
 
 let board = makeBoard();
